7372698ea5c0a843d6dfac8c00b0b378b97dcb1d
b1126b3a826454b57 <189901327+alvarinc@users.noreply.github.com>
2025-01-05 22:07:38 +0100

    basic physics implementation

--- src/KittyEngine.Core/Physics/Collisions/ICollisionManager.cs
+++ src/KittyEngine.Core/Physics/Collisions/ICollisionManager.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows.Media.Media3D;
+
 namespace KittyEngine.Core.Physics.Collisions
 {
     public interface ICollisionManager
@@ -8,5 +10,7 @@ namespace KittyEngine.Core.Physics.Collisions
         StairClimbingResult ComputeStairClimbing(CollisionDetectionParameters parameters, CollisionResult collisionResult);
 
         WallSlidingResult ComputeWallSliding(CollisionDetectionParameters parameters, CollisionResult collisionResult);
+
+        List<Point3D> GetCollidedPoints(CollisionDetectionParameters parameters, CollisionResult collisionResult);
     }
 }
