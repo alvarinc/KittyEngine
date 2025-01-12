f64129432067fb1d3e3902fbddcb00fc97b4fb7a
b1126b3a826454b57 <189901327+alvarinc@users.noreply.github.com>
2025-01-12 17:44:34 +0100

    add startup screen

---
+++ src/KittyEngine.Core/Client/Output/StartupScreen.xaml.cs
@@ -0,0 +1,33 @@
+﻿using KittyEngine.Core.State;
+using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace KittyEngine.Core.Client.Outputs
+{
+    /// <summary>
+    /// Interaction logic for StartupScreen.xaml
+    /// </summary>
+    public partial class StartupScreen : UserControl
+    {
+        private ClientState _clientState;
+
+        public StartupScreen(ClientState clientState)
+        {
+            InitializeComponent();
+
+            _clientState = clientState;
+
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            _clientState.Mode = ClientMode.OutGameMenu;
+            ((DispatcherTimer)sender).Stop();
+        }
+    }
+}
