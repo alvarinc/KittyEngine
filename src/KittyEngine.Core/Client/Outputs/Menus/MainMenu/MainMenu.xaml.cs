d7248c19f134daaa63ca8cb4c2be6af19d879df9
b1126b3a826454b57 <189901327+alvarinc@users.noreply.github.com>
2026-03-26 22:50:24 +0100

    clean code

---
+++ src/KittyEngine.Core/Client/Outputs/Menus/MainMenu/MainMenu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KittyEngine.Core.Client.Outputs.Menus.OutGameMenu
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
